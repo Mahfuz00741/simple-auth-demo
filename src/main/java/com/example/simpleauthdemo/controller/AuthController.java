3c71e00c3b7938da653c1aa8f41a0ff1f0f78420
Mahfuzur Rahman <mahfuz.rahman.swe@gmail.com>
2023-02-01 11:28:12 +0600

    first commit

---
+++ src/main/java/com/example/simpleauthdemo/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.simpleauthdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api")
+public class AuthController {
+
+//    @RequestMapping("/")
+//    public String hello()
+//    {
+//        return "Hello javaTpoint";
+//    }
+
+    @GetMapping(path = "/get")
+    public String getValue() {
+        return "Book";
+    }
+//
+//    @GetMapping(path = "/get-list")
+//    public List<String> getList() {
+//        return Arrays.asList("Book", "pen");
+//    }
+}
